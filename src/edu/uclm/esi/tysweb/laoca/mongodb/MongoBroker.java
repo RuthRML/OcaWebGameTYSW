6bed425a5254dc248930887e1ed031cb61ac64ba
Diego Andérica <dar-96@hotmail.com>
2017-12-08 21:02:02 +0100

    Página de registro redirigda desde index y estilizada con Bootstrap

--- src/edu/uclm/esi/tysweb/laoca/mongodb/MongoBroker.java
+++ src/edu/uclm/esi/tysweb/laoca/mongodb/MongoBroker.java
@@ -16,81 +16,80 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-
 public class MongoBroker {
-	
+
 	private ConcurrentLinkedQueue<MongoClient> usadas, libres;
 	private MongoClient conexionPrivilegiada;
-	
+
 	private MongoBroker() {
-		MongoCredential credenciales=MongoCredential.createCredential("creadorDeUsuarios", 
-				"laoca", "creadorDeUsuarios".toCharArray());
-		ServerAddress address=new ServerAddress("localhost");
-		List<MongoCredential> lista=Arrays.asList(credenciales);
-		this.conexionPrivilegiada=new MongoClient(address, lista);
+		MongoCredential credenciales = MongoCredential.createCredential("creadorDeUsuarios", "laoca",
+				"creadorDeUsuarios".toCharArray());
+		ServerAddress address = new ServerAddress("localhost");
+		List<MongoCredential> lista = Arrays.asList(credenciales);
+		this.conexionPrivilegiada = new MongoClient(address, lista);
 
-		this.usadas=new ConcurrentLinkedQueue<>();
-		this.libres=new ConcurrentLinkedQueue<>();
+		this.usadas = new ConcurrentLinkedQueue<>();
+		this.libres = new ConcurrentLinkedQueue<>();
 
-		credenciales=MongoCredential.createCredential("jugador", "laoca", "jugador".toCharArray());
-		lista=Arrays.asList(credenciales);
-		for (int i=0; i<10; i++) {
-			MongoClient conexion=new MongoClient(address, lista);
+		credenciales = MongoCredential.createCredential("jugador", "laoca", "jugador".toCharArray());
+		lista = Arrays.asList(credenciales);
+		for (int i = 0; i < 10; i++) {
+			MongoClient conexion = new MongoClient(address, lista);
 			this.libres.add(conexion);
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		MongoBroker broker=MongoBroker.get();
+		MongoBroker broker = MongoBroker.get();
 		MongoDatabase db = broker.conexionPrivilegiada.getDatabase("laoca");
 
-		if (db.getCollection("usuarios")==null)
+		if (db.getCollection("usuarios") == null)
 			db.createCollection("usuarios");
 
 		MongoCollection<BsonDocument> usuarios = db.getCollection("usuarios", BsonDocument.class);
 
-		for (int i=1; i<=100; i++) {
-			BsonDocument pepe=new BsonDocument();
+		for (int i = 1; i <= 100; i++) {
+			BsonDocument pepe = new BsonDocument();
 			pepe.put("email", new BsonString("pepe" + i + "@pepe.com"));
 			pepe.put("pwd", new BsonString("pepe"));
 			usuarios.insertOne(pepe);
 		}
 
-		BsonDocument criterio=new BsonDocument();
+		BsonDocument criterio = new BsonDocument();
 		criterio.append("email", new BsonString("pepe100@pepe.com"));
 		FindIterable<BsonDocument> busqueda = usuarios.find(criterio);
 		BsonDocument elementoBuscado = busqueda.first();
 		System.out.println(elementoBuscado.getString("email"));
 		System.out.println(elementoBuscado.getString("pwd"));
 
-		broker.conexionPrivilegiada.close();	
-		
+		broker.conexionPrivilegiada.close();
+
 	}
-	
-	private static class MongoBrokerHolder{
-		static MongoBroker singleton = new MongoBroker();		
+
+	private static class MongoBrokerHolder {
+		static MongoBroker singleton = new MongoBroker();
 	}
 
 	public static MongoBroker get() {
 		return MongoBrokerHolder.singleton;
 	}
-	
-	public  MongoDatabase getDatabase(String databaseName) {
+
+	public MongoDatabase getDatabase(String databaseName) {
 		return conexionPrivilegiada.getDatabase(databaseName);
-		
+
 	}
 
 	public void close() {
-		this.conexionPrivilegiada.close();		
+		this.conexionPrivilegiada.close();
 	}
 
 	public MongoClient getDatabase(String databaseName, String email, String pwd) throws Exception {
-		MongoCredential credenciales=MongoCredential.createCredential(email, databaseName, pwd.toCharArray());
-		ServerAddress address=new ServerAddress("localhost");
-		List<MongoCredential> lista=Arrays.asList(credenciales);
+		MongoCredential credenciales = MongoCredential.createCredential(email, databaseName, pwd.toCharArray());
+		ServerAddress address = new ServerAddress("localhost");
+		List<MongoCredential> lista = Arrays.asList(credenciales);
 		return new MongoClient(address, lista);
 	}
-	
+
 	public MongoClient getConexionPrivilegiada() {
 		return this.conexionPrivilegiada;
 	}
